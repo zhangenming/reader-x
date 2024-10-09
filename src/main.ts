744520f86b5466ea84b9236d663b164e20e0e0e8
zhangenming <282126346@qq.com>
2024-10-09 11:47:42 +0800

    feat boss

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
-import './debug.ts'
+import './debug'
 
 import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
+import { getParams } from './utils.js'
+
+if (getParams().home) {
+  document.documentElement.style.color = 'black'
+}
 
 createApp(App).mount('#app')
