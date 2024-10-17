bccea879f44bf52c7c91ba18014b7f0bb0755f89
zhangenming <282126346@qq.com>
2024-10-17 08:50:58 +0800

    fix minimap

--- src/feat/6miniMap.ts
+++ src/feat/6miniMap.ts
@@ -9,12 +9,12 @@ watchEffect(() => {
   const data = rItemsData[hoverR.value] // vue 监控hoverR值变化事件
 
   if (!data) return // init vue watch
-
+  data.xx
   const q = allLine.at(-1)!.lineIdx
 
   $('#miniMap')!.style.boxShadow = [...new Set(data.map((e) => e.split('-')[0]))] // 去重
-    .map((lineIdx) => (Number(lineIdx) / q) * get屏幕高度())
+    .map((lineIdx) => (allLine[Number(lineIdx)].top / q) * get屏幕高度())
     .map((e) => Math.max(1, Math.floor(e))) //向下取整 但不小于1
     .map((x) => `red 0px ${x}px 0px 0px`)
-    .join(',')
+    .join(',').xx
 })
