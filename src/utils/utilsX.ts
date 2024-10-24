1b409429e38ff23517fe8e64998e005cdb2ca797
zhangenming <282126346@qq.com>
2024-10-24 13:36:34 +0800

    fix random

--- src/utils/utilsX.ts
+++ src/utils/utilsX.ts
@@ -2,7 +2,7 @@ import { 滚动dom } from '../data'
 import { getRandomElement } from './utils'
 
 export function getDomR(dom: HTMLElement) {
-  const rs = dom.getAttribute('rItemsDataKey')?.split('-')
+  const rs = dom.getAttribute('rItemsDataKey')?.split('-').slice(1, -1) // ['-','data','-']
   if (!rs) return
 
   return getRandomElement(rs)
